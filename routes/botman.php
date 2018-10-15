cadb0cd266315ced9f43cd97c5124aba62392c80
Jose Thomas Lopez O <josethomaslopezosorio@gmail.com>
2018-10-15 16:50:31 -0500

    Se añadio el comando de informacion 'about'

--- routes/botman.php
+++ routes/botman.php
@@ -6,4 +6,9 @@ $botman = resolve('botman');
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
+
+$botman->hears('Acerca de'|'about', function ($bot) {
+    $bot->reply('Bot que resuelve todas tus preguntas de Si o No. Desarrollado por Jose Thomas Lopez Osorio.');
+});
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
