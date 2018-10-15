d52dce7ab079b5935e4e9a3be64f61d4d3b1be60
Jose Thomas Lopez O <josethomaslopezosorio@gmail.com>
2018-10-15 16:54:39 -0500

    Se agrego la opcion default

--- routes/botman.php
+++ routes/botman.php
@@ -11,4 +11,8 @@ $botman->hears('Acerca de'|'about', function ($bot) {
     $bot->reply('Bot que resuelve todas tus preguntas de Si o No. Desarrollado por Jose Thomas Lopez Osorio.');
 });
 
+// Opcion default
+$botman->fallback(function($bot) {
+    $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
+});
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
