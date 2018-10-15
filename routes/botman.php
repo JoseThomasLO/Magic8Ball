e8909ad8da4e679e11e69f00329f181e37a7d208
Jose Thomas Lopez O <josethomaslopezosorio@gmail.com>
2018-10-15 17:01:00 -0500

    Se resolvio el bug del about.

--- routes/botman.php
+++ routes/botman.php
@@ -7,12 +7,13 @@ $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 
-$botman->hears('Acerca de'|'about', function ($bot) {
+$botman->hears('Acerca de|about|acerca', function ($bot) {
     $bot->reply('Bot que resuelve todas tus preguntas de Si o No. Desarrollado por Jose Thomas Lopez Osorio.');
 });
 
 // Opcion default
 $botman->fallback(function($bot) {
-    $bot->reply('Sorry, I did not understand these commands. Here is a list of commands I understand: ...');
+    $bot->reply('Lo siento, no entendí tu mensaje. Puedes preguntarme cualquier cosa, ademas del comando about');
 });
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
